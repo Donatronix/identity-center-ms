ded0093421fda21781a6a55fe5ecedc3d2b92672
Dhana <ihor.porokhnenko@gmail.com>
2022-09-04 10:25:49 +0300

    add id to fillable user

--- web/app/Models/User.php
+++ web/app/Models/User.php
@@ -206,6 +206,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var string[]
      */
     protected $fillable = [
+        'id',
         'first_name',
         'last_name',
         'gender',
